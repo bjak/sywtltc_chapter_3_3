9dffe46a657fbbb9dc26cdce43afcaba7ef7f5d0
bjak <blake_jakobsson@utexas.edu>
2017-11-28 17:42:59 -0600

    Failed Test

---
+++ test_stats.py
@@ -0,0 +1,8 @@
+"""Test for Stats Function"""
+
+import stats
+
+def test_stats():
+    assert stats.median([1, 2, 3, 4, 5]) == 3
+    assert stats.median([5, 7, 9, 11, 13]) == 9
+    assert stats.median([3, 6, 9, 12, 15]) == 9
