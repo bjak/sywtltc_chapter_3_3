7fd2b764c49d1b7f85a624a35ef4075f1952ffab
bjak <blake_jakobsson@utexas.edu>
2017-12-12 15:15:33 -0600

    Pylint Clean Test. Still Fails

--- test_stats.py
+++ test_stats.py
@@ -3,6 +3,7 @@
 import stats
 
 def test_stats():
+    """Tests for Stats"""
     assert stats.median([1, 2, 3, 4, 5]) == 3
     assert stats.median([5, 7, 9, 11, 13]) == 9
     assert stats.median([3, 6, 9, 12, 15]) == 9
