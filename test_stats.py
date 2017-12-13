69dcf7b39b9a8e4c0ee78893ee3f89685f0fe0fb
bjak <blake_jakobsson@utexas.edu>
2017-12-13 16:50:31 -0600

    Failed Mean Test

--- test_stats.py
+++ test_stats.py
@@ -12,4 +12,10 @@ def test_stats_range():
     """Tests for Range Function Stats"""
     assert stats.range([1, 2, 3, 4, 5]) == 4
     assert stats.range([5, 10, 15, 20, 25]) == 20
-    assert stats.range([2, 4, 6, 8, 10]) == 8
+    assert stats.range([4, 2, 10, 8, 6, 3, 5, 7, 9, 1]) == 9
+
+def test_stats_mean():
+    """Test for Mean Function Stats"""
+    assert stats.mean([1, 2, 3, 4, 5]) == 3
+    assert stats.mean([3, 6, 9, 12, 15, 18, 21]) == 12
+    assert stats.mean([5, 10, 15, 20, 25, 30]) == 17.5
