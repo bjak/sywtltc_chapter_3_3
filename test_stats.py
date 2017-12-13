e7261d50b87db2249d9a1b4c4a5ad87685842ff6
bjak <blake_jakobsson@utexas.edu>
2017-12-13 16:03:26 -0600

    Failed Test for Range

--- test_stats.py
+++ test_stats.py
@@ -2,8 +2,14 @@
 
 import stats
 
-def test_stats():
-    """Tests for Stats"""
+def test_stats_median():
+    """Tests for Median Function Stats"""
     assert stats.median([1, 2, 3, 4, 5]) == 3
     assert stats.median([5, 7, 9, 11, 13]) == 9
     assert stats.median([3, 6, 9, 12, 15]) == 9
+
+def test_stats_range():
+    """Tests for Range Function Stats"""
+    assert stats.range([1, 2, 3, 4, 5]) == 4
+    assert stats.range([5, 10, 15, 20, 25]) == 20
+    assert stats.range([2, 4, 6, 8, 10]) == 8
